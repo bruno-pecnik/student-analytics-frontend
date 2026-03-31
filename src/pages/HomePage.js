c922200adf3ccfff5688621613093ae460016743
Bruno Pecnik <bruno.pecnik@fer.hr>
2026-04-01 00:58:25 +0200

    updated HomePage with dashboard cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Typography } from 'antd';
+import { Card, Row, Col, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 function HomePage() { // jsx koji react pretvori u html
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <div style={{ padding: 24 }}>
-      <Title level={2}>Dobrodošli!</Title>
-      <p>Home stranica - u izradi.</p>
+      <Title level={2}>Dobrodošli, {user?.firstName}!</Title>
+      <Text type="secondary">Što želite pogledati?</Text>
+
+      <Row gutter={16} style={{ marginTop: 24 }}>
+        <Col xs={24} sm={12} md={8}>
+          <Card
+            title="Rezultati"
+            hoverable
+            onClick={() => navigate('/rezultati')}
+          >
+            <Text>Pregledajte rezultate i statistike.</Text>
+          </Card>
+        </Col>
+
+        <Col xs={24} sm={12} md={8}>
+          <Card
+            title="Profil"
+            hoverable
+            onClick={() => navigate('/profil')}
+          >
+            <Text>Pregledajte i uredite svoj profil.</Text>
+          </Card>
+        </Col>
+
+        {user?.role === 'ADMIN' && (
+          <Col xs={24} sm={12} md={8}>
+            <Card
+              title="Admin Panel"
+              hoverable
+              onClick={() => navigate('/admin')}
+            >
+              <Text>Upravljajte korisnicima i kolegijima.</Text>
+            </Card>
+          </Col>
+        )}
+      </Row>
     </div>
   );
 }
